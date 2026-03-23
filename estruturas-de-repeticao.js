8f47cad2e25a0dd6c59f4b544fab643854d61833
Rapix27 <rapix24052011@gmail.com>
2026-03-23 10:22:00 -0300

    while

---
+++ estruturas-de-repeticao.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+let numero = 7
+let tabuada = 0
+let resultado = 0
+
+while(resultado <= numero *10){
+    console.log(resultado)
+    tabuada = tabuada + 1
+    resultado = tabuada * numero
+}
